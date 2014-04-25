426a92935ae8d80c102b5c0263f703774f1a01a0
Patrick Stegmann <code@patrick-stegmann.de>
2014-04-25 19:31:30 +0200

    Modified license information.
    @CommerceHub: If I made a mistake somewhere, please notify me. First time I forked a project :P

--- src/main/java/com/commercehub/bamboo/plugins/gitlab/GitLabWebRepositoryViewer.java
+++ src/main/java/com/commercehub/bamboo/plugins/gitlab/GitLabWebRepositoryViewer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 CommerceHub and others
+ * Copyright 2013 CommerceHub and Patrick Stegmann
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
